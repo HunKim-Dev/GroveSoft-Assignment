9894c8116ecd5f05a595a9fb26725e1bb31909f4
김태헌 <7w7w7w7w@naver.com>
2025-12-18 16:15:57 +0900

    init: GroveSoft-Assignment 기본 세팅

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import CountDownTimer from './components/CountDownTimer.vue';
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1>신규 서비스 오픈 기념 이벤트</h1>
+    </div>
+    <div>
+      <p>응모하고 특별 혜택을 받아가세요.</p>
+    </div>
+  </header>
+
+  <main>
+    <CountDownTimer />
+  </main>
+</template>
