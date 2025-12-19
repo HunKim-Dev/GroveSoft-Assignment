33921ea192a3e0f5be0eac2c552a25e40d05ff60
김태헌 <7w7w7w7w@naver.com>
2025-12-19 12:05:32 +0900

    feat: 이벤트 ui 헤더 설명 및 실시간 날짜로 기간 지정 기능 구현

---
+++ src/components/EventHeaderInfo.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const formatDate = (date: Date) =>
+  date.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+const formatHourMinute = (date: Date) =>
+  date.toLocaleTimeString("ko-KR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+const addMinutes = (date: Date, minutes: number) => new Date(date.getTime() + minutes * 60 * 1000);
+
+const today = new Date();
+const DAY_MINUTES = 1440;
+const limit = addMinutes(today, DAY_MINUTES);
+
+const currentDate = `${formatDate(today)}`;
+const limitDate = `${formatDate(limit)} ${formatHourMinute(limit)}`;
+</script>
+
+<template>
+  <div class="mt-20 mb-2">
+    <h1 class="text-4xl font-black">그로브 뷰티 오픈 기념 이벤트</h1>
+  </div>
+
+  <div class="mb-2">
+    <p class="text-base font-bold">기간 : {{ currentDate }} - {{ limitDate }} 까지</p>
+  </div>
+
+  <div class="mb-15">
+    <p>응모하고 특별 혜택을 받아가세요.</p>
+  </div>
+</template>
