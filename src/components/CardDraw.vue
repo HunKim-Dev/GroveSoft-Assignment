b9ad48fe3417f6f8f93f28f12e5497778df5507b
김태헌 <7w7w7w7w@naver.com>
2025-12-22 13:36:12 +0900

    feat: 카드 뽑기 인터렉션 기능 구현

---
+++ src/components/CardDraw.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+type Card = { id: number; fornt: string; drawn: boolean };
+
+const CARDS = ref<Card[]>([
+  { id: 1, fornt: "10%", drawn: false },
+  { id: 2, fornt: "10%", drawn: false },
+  { id: 3, fornt: "10%", drawn: false },
+  { id: 4, fornt: "10%", drawn: false },
+  { id: 5, fornt: "10%", drawn: false },
+]);
+
+const shuffleCard = () => {
+  const card = CARDS.value;
+  if (card.length < 2) return;
+
+  for (let i = card.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    const temp = card[i]!;
+    card[i] = card[j]!;
+    card[j] = temp;
+  }
+};
+
+const hasDrawn = ref(false);
+
+const flipCard = (id: number) => {
+  if (hasDrawn.value) return;
+
+  const target = CARDS.value.find((card) => card.id === id);
+
+  if (!target) return;
+
+  target.drawn = true;
+  hasDrawn.value = true;
+};
+</script>
+
+<template>
+  <div>
+    <div class="mb-4">
+      <p class="text-2xl">카드를 뽑아서 할인 쿠폰을 더 받아 가세요!</p>
+    </div>
+  </div>
+
+  <transition-group name="shuffle" tag="div" class="mb-10 flex flex-wrap gap-3">
+    <div
+      v-for="card in CARDS"
+      :key="card.id"
+      class="card cursor-pointer transition hover:-translate-y-3 hover:shadow-lg"
+      :class="{ 'card--flipped': card.drawn }"
+      @click="flipCard(card.id)"
+    >
+      <div class="card__inner">
+        <div class="card__face card__back">?</div>
+        <div class="card__face card__front">{{ card.fornt }}</div>
+      </div>
+    </div>
+  </transition-group>
+
+  <div class="flex justify-center mt-6">
+    <button
+      @click="shuffleCard"
+      class="px-5 py-3 cursor-pointer text-sm font-semibold rounded-md border bg-[#007AFF] text-white transition hover:bg-[#004085]"
+    >
+      카드 섞기
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 75px;
+  height: 100px;
+  border: 1px solid #d1d5db;
+  border-radius: 8px;
+  background-color: transparent;
+  perspective: 800px;
+}
+
+.card__inner {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform-style: preserve-3d;
+  transition: transform 0.45s;
+}
+
+.card__face {
+  position: absolute;
+  inset: 0;
+  border-radius: 8px;
+  backface-visibility: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 600;
+  font-size: 18px;
+}
+
+.card__back {
+  background: #001541;
+  color: #ffffff;
+}
+
+.card__front {
+  background: #ffffff;
+  color: #111827;
+  transform: rotateY(180deg);
+}
+
+.card--flipped .card__inner {
+  transform: rotateY(180deg);
+}
+
+.shuffle-move {
+  transition: transform 0.7s;
+}
+</style>
