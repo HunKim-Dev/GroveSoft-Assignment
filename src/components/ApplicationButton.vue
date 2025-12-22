dc45d8a75c09c94416198bfa9e278d27bab95286
김태헌 <7w7w7w7w@naver.com>
2025-12-23 01:43:37 +0900

    feat: 응모하기 버튼 기능 구현

---
+++ src/components/ApplicationButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ApplicationFrom from "./ApplicationFrom.vue";
+
+const openApplicationFrom = ref(false);
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <button
+      @click="openApplicationFrom = true"
+      class="px-30 py-5 cursor-pointer text-2xl font-bold rounded-full border bg-[#007AFF] text-white transition hover:bg-[#004085]"
+    >
+      응모하기
+    </button>
+  </div>
+  <ApplicationFrom v-if="openApplicationFrom" @close="openApplicationFrom = false" />
+</template>
+
+<style scoped></style>
